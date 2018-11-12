f385d3f84fe4e018327919c0c9461e2ad2e861d9
Rafał Kotowicz <rafal.the.cat@gmail.com>
2018-11-12 19:28:58 +0100

    [CJM][S02] hello world 100th time

---
+++ udemy_complete_java_masterclass/src/section_02/HelloWorld.java
@@ -0,0 +1,8 @@
+package section_02;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Why the fuck am I doing this for 100th time :|");
+    }
+}
