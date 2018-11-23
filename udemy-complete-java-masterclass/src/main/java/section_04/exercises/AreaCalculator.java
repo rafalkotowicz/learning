fe8789eb8e7f3e2e3f239a7ae65c1ef334d61984
Rafał Kotowicz <rafal.the.cat@gmail.com>
2018-11-23 21:11:09 +0100

    [CJM][S04] A new exercise done

---
+++ udemy-complete-java-masterclass/src/main/java/section_04/exercises/AreaCalculator.java
@@ -0,0 +1,13 @@
+package section_04.exercises;
+
+public class AreaCalculator {
+
+    public static double area(double radius) {
+        final double PI = 3.141592;
+        return radius >= 0 ? PI * radius * radius : -1.0d;
+    }
+
+    public static double area(double x, double y) {
+        return x >= 0  && y >= 0 ? x * y : -1.0d;
+    }
+}
