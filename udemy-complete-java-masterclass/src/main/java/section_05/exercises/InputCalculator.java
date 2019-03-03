83d22a32836feee86624fcdbd9bc1198e922421c
Rafał Kotowicz <rafal.the.cat@gmail.com>
2019-03-03 21:37:05 +0100

    [CJM][S05] InputCalculator exercise finished

--- udemy-complete-java-masterclass/src/main/java/section_05/exercises/InputCalculator.java
+++ udemy-complete-java-masterclass/src/main/java/section_05/exercises/InputCalculator.java
@@ -1,7 +1,22 @@
 package section_05.exercises;
 
-public class InputCalculator {
-    public void inputThenPrintSumAndAverage(){
+import java.util.Scanner;
 
+public class InputCalculator {
+    public static void main(String... args) {
+        //public static void inputThenPrintSumAndAverage() {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int sum = 0;
+        int inputCounter = 0;
+        while (scanner.hasNextInt()) {
+            sum += scanner.nextInt();
+            inputCounter += 1;
+
+        }
+
+        System.out.println("SUM = " + sum + " AVG = " + Math.round((double)sum / inputCounter));
+        scanner.close();
     }
 }
