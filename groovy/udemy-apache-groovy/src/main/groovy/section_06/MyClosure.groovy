e41d51aaf56f58ecc0f2bc47303087512ab2fcf3
Rafał Kotowicz <rafal.the.cat@gmail.com>
2019-11-13 19:39:10 +0100

    [Udemy][Groovy][S06] Closure exercise - part 2

--- groovy/udemy-apache-groovy/src/main/groovy/section_06/MyClosure.groovy
+++ groovy/udemy-apache-groovy/src/main/groovy/section_06/MyClosure.groovy
@@ -2,6 +2,17 @@ package section_06
 
 class MyClosure {
 
-    static Closure<Integer> summingClosure = { int a, int b, int c -> a + b + c }
-    static Closure<Boolean> isDigit = { String p -> p.matches("[0-9]*") }
+    static Closure<Integer> summingClosure = { int a = 0, int b = 0, int c = 0 -> a + b + c }
+    static Closure<Boolean> isNumber = { String p -> p.matches("[0-9]*") }
+    static Closure printMap = { def key, def value -> return ("$key:= $value") }
+
+    static Closure<BigDecimal> calculatePaintPrice = {
+        def areaToPaint,
+        def paintEfficiencyFromLiter,
+        def paintPricePerLiter ->
+            areaToPaint / paintEfficiencyFromLiter * paintPricePerLiter
+    }
+
+    static Closure calculatePaintPriceFixedArea = calculatePaintPrice.curry(10.0)
+    static Closure calculatePaintPriceFixedPrice = calculatePaintPrice.ncurry(2, 4.0)
 }
--- groovy/udemy-apache-groovy/src/test/groovy/section_06/MyClosureTest.groovy
+++ groovy/udemy-apache-groovy/src/test/groovy/section_06/MyClosureTest.groovy
@@ -2,19 +2,37 @@ package section_06
 
 import org.junit.Test
 
+import static section_06.MyClosure.*
+
 class MyClosureTest {
 
     @Test
-    void summingClosureSums3ArgumentsTest() {
-        assert 8 == MyClosure.summingClosure(1, 2, 5)
+    void summingClosureTest() {
+        assert 8 == summingClosure(1, 2, 5)
+        assert 3 == summingClosure(1, 2)
+        assert 4 == summingClosure(0, 4)
+        assert 3 == summingClosure(-1, 4)
+        assert -1 == summingClosure(-1)
     }
 
     @Test
     void isDigitPositiveTest() {
-        assert MyClosure.isDigit("1")
         List<String> toCheck = ["1", "222", "123123123"]
-        Closure shouldBeTrue = { assert MyClosure.isDigit(it) }
-        toCheck.forEach(shouldBeTrue)
+        toCheck.forEach({ assert isNumber(it) })
     }
 
+    @Test
+    void applyClosureToMapTest() {
+        def testData = [surname: "Denis", familyname: "Rodman"]
+        testData.forEach({ def key, def value ->
+            assert printMap(key, value).matches(".*name:.*")
+        })
+    }
+
+    @Test
+    void curryTest() {
+        assert 20.0 == calculatePaintPrice.call(10.0, 2.0, 4.0)
+        assert 20.0 == calculatePaintPriceFixedArea(2.0, 4.0)
+        assert 20.0 == calculatePaintPriceFixedPrice(10.0, 2.0)
+    }
 }
