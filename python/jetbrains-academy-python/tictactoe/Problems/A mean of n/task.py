7fcfaeafd429db679433bbd430208be0e1848e51
Rafał Kotowicz <rafal.the.cat@gmail.com>
2020-05-14 22:42:10 +0200

    [JetBrainsAcademy][Python][Tic-Tac-Toe] Stage 3/5 done. Additional problems solved.

---
+++ python/jetbrains-academy-python/tictactoe/Problems/A mean of n/task.py
@@ -0,0 +1,5 @@
+sum = 0
+count = int(input().strip())
+for _i in range(count):
+    sum += int(input().strip())
+print(sum / count)
--- python/jetbrains-academy-python/tictactoe/Tic-Tac-Toe/task/tictactoe/tictactoe.py
+++ python/jetbrains-academy-python/tictactoe/Tic-Tac-Toe/task/tictactoe/tictactoe.py
@@ -1,5 +1,3 @@
-import sys
-
 user_input = input()
 board = [[user_input[0], user_input[1], user_input[2]],
          [user_input[3], user_input[4], user_input[5]],
@@ -12,19 +10,20 @@ max_cols = 3
 curr_col = 0
 while curr_row < max_rows:
     while curr_col < max_cols:
-        if(curr_col == 0):
-            print("|",end="")
+        if curr_col == 0:
+            print("|", end="")
             print(" ", end="")
         print(board[curr_row][curr_col], end="")
         print(" ", end="")
-        if(curr_col == max_cols - 1):
-            print("|",end="")
+        if curr_col == max_cols - 1:
+            print("|", end="")
         curr_col += 1
     print()
     curr_row += 1
     curr_col = 0
 print("---------")
 
+who_won = []
 # win condition detection - row
 x, o = 0, 0
 for row in range(max_rows):
@@ -34,11 +33,9 @@ for row in range(max_rows):
         elif board[row][col] == "O":
             o += 1
     if x == 3:
-        print("X wins")
-        sys.exit()
+        who_won.append("X wins")
     if o == 3:
-        print("O wins")
-        sys.exit()
+        who_won.append("O wins")
     x, o = 0, 0
 
 # win condition detection - column
@@ -50,15 +47,13 @@ for col in range(max_cols):
         elif board[row][col] == "O":
             o += 1
     if x == 3:
-        print("X wins")
-        sys.exit()
+        who_won.append("X wins")
     if o == 3:
-        print("O wins")
-        sys.exit()
+        who_won.append("O wins")
     x, o = 0, 0
 
 # win condition detection - diagonal \
-    x, o = 0, 0
+x, o = 0, 0
 for col in range(max_cols):
     for row in range(max_rows):
         if row == col and board[row][col] == "X":
@@ -66,13 +61,27 @@ for col in range(max_cols):
         elif row == col and board[row][col] == "O":
             o += 1
     if x == 3:
-        print("X wins")
-        sys.exit()
+        who_won.append("X wins")
     if o == 3:
-        print("O wins")
-        sys.exit()
+        who_won.append("O wins")
 
+x_moves = len([x for row in range(max_rows) for col in range(max_cols) if board[row][col] == "X"])
+o_moves = len([x for row in range(max_rows) for col in range(max_cols) if board[row][col] == "O"])
+game_finished = (x_moves + o_moves) == 9
 # win condition detection - diagonal /
 # [0, 2]
 # [1, 1]
 # [2, 0]
+if board[0][2] == "X" and board[1][1] == "X" and board[2][0] == "X":
+    who_won.append("X wins")
+elif board[0][2] == "O" and board[1][1] == "O" and board[2][0] == "O":
+    who_won.append("O wins")
+
+if len(who_won) == 0 and game_finished:
+    print("Draw")
+elif len(who_won) > 1 or abs(x_moves - o_moves) >= 2:
+    print("Impossible")
+elif len(who_won) == 0 and not game_finished:
+    print("Game not finished")
+elif len(who_won) == 1:
+    print(who_won[0])
