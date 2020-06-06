e61306c7b09e1ac3b657e709bb5ded96b64c9f07
Rafał Kotowicz <rafal.the.cat@gmail.com>
2020-06-06 22:23:44 +0200

    [JetBrainsAcademy][Python][CreditCalculator] Stage 2/4 done.

--- python/jetbrains-academy-python/creditcalculator/Credit Calculator/task/creditcalc/creditcalc.py
+++ python/jetbrains-academy-python/creditcalculator/Credit Calculator/task/creditcalc/creditcalc.py
@@ -1,10 +1,28 @@
-credit_principal = 'Credit principal: 1000'
-first_month = 'Month 1: paid out 250'
-second_month = 'Month 2: paid out 250'
-third_month = 'Month 3: paid out 500'
-final_output = 'The credit has been repaid!'
-messages = [credit_principal, first_month, second_month, third_month, final_output]
+import math
 
-for message in messages:
-    print(message)
+print("Enter the credit principal:")
+credit_principal = int(input())
+print("What do you want to calculate")
+print('type "m" - for count of months,')
+print('type "p" - for monthly payment:')
+action = input()
 
+if action == "m":
+    print("Enter monthly payment:")
+    payment = int(input())
+    months = math.ceil(credit_principal / payment)
+    if months == 1:
+        print("It takes 1 month to repay the credit")
+    else:
+        print("It takes", months, "months to repay the credit")
+
+if action == "p":
+    print("Enter count of months:")
+    months = int(input())
+    payment = math.ceil(credit_principal / months)
+    last_payment = 0
+    if payment * months > credit_principal:
+        last_payment = credit_principal - payment * (months - 1)
+        print("Your monthly payment =", payment, "with last month payment =", last_payment)
+    else:
+        print("Your monthly payment =", payment)
