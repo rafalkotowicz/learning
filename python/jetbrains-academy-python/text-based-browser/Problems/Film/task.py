7e5faa9c25ae995482988b3613ff19dd0f62c2f9
Rafał Kotowicz <rafal.the.cat@gmail.com>
2020-06-27 01:42:47 +0200

    [JetBrainsAcademy][Python][TextBasedBrowser] Stage 2/6 ongoing. Some additional topics/problems done.

---
+++ python/jetbrains-academy-python/text-based-browser/Problems/Film/task.py
@@ -0,0 +1,4 @@
+title = input()
+director = input()
+release_date = input()
+print(f"{title} (dir. {director}) came out in {release_date}")
--- python/jetbrains-academy-python/text-based-browser/Problems/Process integer input/task.py
+++
@@ -1,8 +0,0 @@
-while True:
-    number = int(input())
-    if number < 10:
-        continue
-    elif number > 100:
-        break
-    else:
-        print(number)
