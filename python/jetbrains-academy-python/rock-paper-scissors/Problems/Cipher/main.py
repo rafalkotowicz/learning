7e21b17de8aabe0b999cb31f6e96803785f9f9d4
Rafał Kotowicz <rafal.the.cat@gmail.com>
2020-11-25 00:49:40 +0100

    [JetBrainsAcademy][Python][RockPaperScissors] Stage 5/5 - ONGOING. Some topics and problems done.

---
+++ python/jetbrains-academy-python/rock-paper-scissors/Problems/Cipher/main.py
@@ -0,0 +1,4 @@
+string = "no clouds here to spy on pets"
+string_2 = string[::5]
+string_3 = string_2[::-1]
+print(string_3)
---
+++ python/jetbrains-academy-python/rock-paper-scissors/Problems/You've got mail_/main.py
@@ -0,0 +1,4 @@
+# write your code here
+# remember: the variable email is already defined
+# email = input()
+print(email[:-15])
