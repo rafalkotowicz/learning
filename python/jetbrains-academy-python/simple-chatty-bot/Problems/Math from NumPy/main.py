3b63703c58045a973721fbf95ffa9451ccb2cbc8
Rafał Kotowicz <rafal.the.cat@gmail.com>
2021-04-05 22:32:48 +0200

    [JetBrainsAcademy][Python][NONE] some problems solved and 1 topic completed

---
+++ python/jetbrains-academy-python/simple-chatty-bot/Problems/Math from NumPy/main.py
@@ -0,0 +1,7 @@
+from math import cos, radians
+
+
+def calculate_cosine(angle_in_degrees):
+    # do not forget to round the result and print it
+    print(round(cos(radians(angle_in_degrees)), 2))
+
