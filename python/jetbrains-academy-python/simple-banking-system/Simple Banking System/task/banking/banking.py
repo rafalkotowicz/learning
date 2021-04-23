4245d53d12a8f1f0942283186f50430a3f97f660
Rafał Kotowicz <rafal.the.cat@gmail.com>
2021-04-23 02:01:10 +0200

    [JetBrainsAcademy][Python][SimpleBankingSystem] Stage 4/4 done!

--- python/jetbrains-academy-python/simple-banking-system/Simple Banking System/task/banking/banking.py
+++ python/jetbrains-academy-python/simple-banking-system/Simple Banking System/task/banking/banking.py
@@ -73,13 +73,34 @@ def login_gui():
 def logged_gui(card):
     while True:
         print("1. Balance")
-        print("2. Log out")
+        print("2. Add income")
+        print("3. Do transfer")
+        print("4. Close account")
+        print("5. Log out")
         print("0. Exit")
         choice = int(input())
 
         if 1 == choice:
             print(f'Balance: {card.balance}')
         elif 2 == choice:
+            card.add_income(int(input("Enter income:")))
+            print("Income was added!")
+        elif 3 == choice:
+            print("Transfer")
+            target_card_number = input("Enter card number:")
+            target_card = card.is_card_number_valid(target_card_number)
+            if target_card:
+                amount_to_transfer = int(input("Enter how much money you want to transfer:"))
+                if card.has_enough_money(amount_to_transfer):
+                    card.transfer_money(target_card, amount_to_transfer)
+                    print("Success!")
+                else:
+                    print("Not enough money!")
+
+        elif 4 == choice:
+            card.close_acount()
+            bank_gui()
+        elif 5 == choice:
             bank_gui()
         elif 0 == choice:
             print("Bye!")
@@ -94,7 +115,7 @@ class CreditCard:
         self.card_number = None
         self.pin = None
         self.balance = 0
-        if (kwargs.get("is_new", True)):
+        if kwargs.get("is_new", True):
             self.create_new_card()
             self.write_to_db()
             CreditCard.all_cards.append(self)
@@ -105,7 +126,7 @@ class CreditCard:
     def create_new_card(self):
         self.account_number = random.randint(100000000, 999999999)
         self.card_number_no_luhn = CreditCard.IIN * 10000000000 + self.account_number * 10
-        self.luhn = self.calculate_luhn()
+        self.luhn = self.calculate_luhn(self.card_number_no_luhn)
         self.card_number = self.card_number_no_luhn + self.luhn
         self.pin = random.randint(1000, 9999)
         print("Your card has been created")
@@ -123,8 +144,8 @@ class CreditCard:
     def __repr__(self):
         return "Card number: " + str(self.card_number) + " PIN: " + str(self.pin)
 
-    def calculate_luhn(self):
-        card_number_str = str(self.card_number_no_luhn)
+    def calculate_luhn(self, card_number_no_luhn) -> int:
+        card_number_str = str(card_number_no_luhn)
         digit_sum = 0
         for i in range(1, 16):
             current_digit = int(card_number_str[i - 1])
@@ -136,10 +157,55 @@ class CreditCard:
                     digit_sum -= 9
         return 0 if digit_sum % 10 == 0 else 10 - digit_sum % 10
 
+    def is_luhn_valid(self, card_number_to_check) -> bool:
+        actual_luhn = int(card_number_to_check) % 10
+        expected_luhn = self.calculate_luhn(card_number_to_check)
+        return actual_luhn == expected_luhn
+
     def write_to_db(self):
         dc.execute_query(f"""
             INSERT INTO card (number, pin) 
             VALUES ({self.card_number}, {self.pin});
+        """)
+
+    def add_income(self, new_balance):
+        self.balance += new_balance
+        self.update_balance_in_db()
+
+    def update_balance_in_db(self):
+        dc.execute_query(f"""
+            UPDATE card 
+            SET balance = {self.balance} 
+            WHERE number = {self.card_number};
+        """)
+
+    def is_card_number_valid(self, target_card_number) -> bool:
+        if target_card_number == self.card_number:
+            print("You can't transfer money to the same account!")
+            return False
+        if not self.is_luhn_valid(target_card_number):
+            print("Probably you made a mistake in the card number. Please try again!")
+            return False
+        target_card = find_card_in_db(target_card_number)
+        if "CardNotFound" == target_card:
+            print("Such a card does not exist.")
+            return False
+        else:
+            return target_card
+
+    def has_enough_money(self, amount_to_transfer) -> bool:
+        return self.balance >= amount_to_transfer
+
+    def transfer_money(self, target_card, amount_to_transfer) -> None:
+        self.balance -= amount_to_transfer
+        self.update_balance_in_db()
+        target_card.balance += amount_to_transfer
+        target_card.update_balance_in_db()
+
+    def close_acount(self) -> None:
+        dc.execute_query(f"""
+            DELETE FROM card 
+            WHERE number = {self.card_number};
         """)
 
 
