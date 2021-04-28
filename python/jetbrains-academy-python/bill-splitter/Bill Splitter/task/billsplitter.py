48184ceab7812ae227d97a901dd5a3008b709815
Rafał Kotowicz <rafal.the.cat@gmail.com>
2021-04-29 01:34:54 +0200

    [JetBrainsAcademy][Python][BillSplitter] Stage 2/4 Done!

--- python/jetbrains-academy-python/bill-splitter/Bill Splitter/task/billsplitter.py
+++ python/jetbrains-academy-python/bill-splitter/Bill Splitter/task/billsplitter.py
@@ -9,5 +9,12 @@ print("Enter the name of every friend (including you), each on a new line:")
 for i in range(0, no_participants):
     participants_list.append(input())
 
-participants_dict = dict.fromkeys(participants_list, 0)
+participants_dict: dict[str, int] = dict.fromkeys(participants_list, 0)
+
+print("Enter the total bill value: ")
+bill_total = int(input())
+for participant in participants_dict:
+    participants_dict[participant] = round(bill_total / len(
+        participants_list), 2)
+
 print(participants_dict)
