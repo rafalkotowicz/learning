eb00cf04cc652ca8659b1b8ff6d3bbbb608f333a
Rafał Kotowicz <rafal.the.cat@gmail.com>
2021-04-29 01:48:45 +0200

    [JetBrainsAcademy][Python][BillSplitter] Stage 3/4 Done!

--- python/jetbrains-academy-python/bill-splitter/Bill Splitter/task/billsplitter.py
+++ python/jetbrains-academy-python/bill-splitter/Bill Splitter/task/billsplitter.py
@@ -1,3 +1,5 @@
+import random
+
 print("Enter the number of friends joining (including you):")
 no_participants = int(input())
 if no_participants < 1:
@@ -13,8 +15,25 @@ participants_dict: dict[str, int] = dict.fromkeys(participants_list, 0)
 
 print("Enter the total bill value: ")
 bill_total = int(input())
-for participant in participants_dict:
-    participants_dict[participant] = round(bill_total / len(
-        participants_list), 2)
 
-print(participants_dict)
+print('Do you want to use the "Who is lucky?" feature? Write Yes/No:')
+lucky_input = input()
+who_is_lucky = None
+if "Yes" == lucky_input:
+    who_is_lucky = random.choice(participants_list)
+    print(f"{who_is_lucky} is the lucky one!")
+else:
+    print("No one is going to be lucky")
+
+for participant in participants_dict:
+    if who_is_lucky:
+        if participant != who_is_lucky:
+            participants_dict[participant] = round(bill_total / (len(
+                participants_list) - 1), 2)
+        else:
+            participants_dict[participant] = 0
+    else:
+        participants_dict[participant] = round(bill_total / len(
+            participants_list), 2)
+
+# print(participants_dict)
