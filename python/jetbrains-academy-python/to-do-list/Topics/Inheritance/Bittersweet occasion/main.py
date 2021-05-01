ba5801c15018b9a4186fc8b8eb4429fba4747588
Rafał Kotowicz <rafal.the.cat@gmail.com>
2021-05-01 17:59:37 +0200

    [JetBrainsAcademy][Python][BillSplitter][1/4] Topic: Inheritance

---
+++ python/jetbrains-academy-python/to-do-list/Topics/Inheritance/Bittersweet occasion/main.py
@@ -0,0 +1,7 @@
+def find_the_parent(child):
+    if issubclass(child, Drinks):
+        print("Drinks")
+    elif issubclass(child, Pastry):
+        print("Pastry")
+    elif issubclass(child, Sweets):
+        print("Sweets")
---
+++ python/jetbrains-academy-python/to-do-list/Topics/Inheritance/SuperRobot/main.py
@@ -0,0 +1,4 @@
+print("True" if issubclass(SuperRobot, AstromechDroid) else "False")
+print("True" if issubclass(SuperRobot, MedicalDroid) else "False")
+print("True" if issubclass(SuperRobot, BattleDroid) else "False")
+print("True" if issubclass(SuperRobot, PilotDroid) else "False")
