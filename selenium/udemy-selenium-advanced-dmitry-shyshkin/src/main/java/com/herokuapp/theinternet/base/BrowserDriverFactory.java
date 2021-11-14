cba29b1b7814121399943f0a9005e7f365e9c860
Rafał Kotowicz <rafal.the.cat@gmail.com>
2021-11-14 23:57:55 +0100

    [Udemy][Selenium][Advanced-Shyshkin] Section 2: DONE. Set up logging (lof4j2).

--- selenium/udemy-selenium-advanced-dmitry-shyshkin/src/main/java/com/herokuapp/theinternet/base/BrowserDriverFactory.java
+++ selenium/udemy-selenium-advanced-dmitry-shyshkin/src/main/java/com/herokuapp/theinternet/base/BrowserDriverFactory.java
@@ -1,5 +1,6 @@
 package com.herokuapp.theinternet.base;
 
+import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -7,13 +8,15 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 public class BrowserDriverFactory {
     private final ThreadLocal<WebDriver> driver = new ThreadLocal<>();
     private final String browser;
+    private Logger log;
 
-    public BrowserDriverFactory(String browser) {
+    public BrowserDriverFactory(String browser, Logger log) {
         this.browser = browser.toLowerCase();
+        this.log = log;
     }
 
     public WebDriver createDriver() {
-        System.out.println("Create driver: " + browser);
+        log.info("Create driver: " + browser);
 
         switch (browser) {
             case "chrome" -> {
@@ -25,7 +28,7 @@ public class BrowserDriverFactory {
                 driver.set(new FirefoxDriver());
             }
             default -> {
-                System.out.println("Do not know how to start: " + browser + ", starting chrome.");
+                log.info("Do not know how to start: " + browser + ", starting chrome.");
                 System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
                 driver.set(new ChromeDriver());
             }
