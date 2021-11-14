5a706200af3359d848d287981419bd7e6c8b4534
Rafał Kotowicz <rafal.the.cat@gmail.com>
2021-11-14 23:18:37 +0100

    [Udemy][Selenium][Advanced-Shyshkin] Section 2: Packaging with brain. Parallel execution of tests.

--- selenium/udemy-selenium-advanced-dmitry-shyshkin/src/main/java/com/herokuapp/theinternet/base/BrowserDriverFactory.java
+++ selenium/udemy-selenium-advanced-dmitry-shyshkin/src/main/java/com/herokuapp/theinternet/base/BrowserDriverFactory.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class BrowserDriverFactory {
     private final ThreadLocal<WebDriver> driver = new ThreadLocal<>();
-    private String browser;
+    private final String browser;
 
     public BrowserDriverFactory(String browser) {
         this.browser = browser.toLowerCase();
@@ -16,21 +16,19 @@ public class BrowserDriverFactory {
         System.out.println("Create driver: " + browser);
 
         switch (browser) {
-            case "chrome":
+            case "chrome" -> {
                 System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
                 driver.set(new ChromeDriver());
-                break;
-
-            case "firefox":
+            }
+            case "firefox" -> {
                 System.setProperty("webdriver.gecko.driver", "src/main/resources/geckodriver.exe");
                 driver.set(new FirefoxDriver());
-                break;
-
-            default:
+            }
+            default -> {
                 System.out.println("Do not know how to start: " + browser + ", starting chrome.");
                 System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
                 driver.set(new ChromeDriver());
-                break;
+            }
         }
 
         return driver.get();
