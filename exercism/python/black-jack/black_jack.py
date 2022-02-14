3e9bbca86e8a2e45db10284264039974322dac53
Rafał Kotowicz <rafal.the.cat@gmail.com>
2022-02-14 18:52:44 +0100

    [Exercism][Python][BlackJack] Small Refactoring.

--- exercism/python/black-jack/black_jack.py
+++ exercism/python/black-jack/black_jack.py
@@ -8,7 +8,7 @@ CARDS_10 = FACE_CARDS + ['10']
 CARDS_10_SCORE = 10
 
 
-def value_of_card(card):
+def value_of_card(card: str) -> int:
     """Determine the scoring value of a card.
 
     :param card: str - given card.
@@ -25,10 +25,11 @@ def value_of_card(card):
     return value
 
 
-def higher_card(card_one, card_two):
+def higher_card(card_one: str, card_two: str):
     """Determine which card has a higher value in the hand.
 
-    :param card_one, card_two: str - cards dealt. 'J', 'Q', 'K' = 10; 'A' = 1; numerical value otherwise.
+    :param card_one: str - card dealt. 'J', 'Q', 'K' = 10; 'A' = 1; numerical value otherwise.
+    :param card_two: str - card dealt. 'J', 'Q', 'K' = 10; 'A' = 1; numerical value otherwise.
     :return: higher value card - str. Tuple of both cards if they are of equal value.
     """
 
@@ -43,12 +44,11 @@ def higher_card(card_one, card_two):
         return card_one, card_two
 
 
-def value_of_ace(card_one, card_two):
+def value_of_ace(card_one: str, card_two: str) -> int:
     """Calculate the most advantageous value for the ace card.
 
-    :param card_one, card_two: str - card dealt. 'J', 'Q', 'K' = 10;
-           'A' = 11 (if already in hand); numerical value otherwise.
-
+    :param card_one: str - card dealt. 'J', 'Q', 'K' = 10; 'A' = 1; numerical value otherwise.
+    :param card_two: str - card dealt. 'J', 'Q', 'K' = 10; 'A' = 1; numerical value otherwise.
     :return: int - value of the upcoming ace card (either 1 or 11).
     """
 
@@ -61,30 +61,33 @@ def value_of_ace(card_one, card_two):
         return 1
 
 
-def is_blackjack(card_one, card_two):
+def is_blackjack(card_one: str, card_two: str) -> bool:
     """Determine if the hand is a 'natural' or 'blackjack'.
 
-    :param card_one, card_two: str - cards dealt. 'J', 'Q', 'K' = 10; 'A' = 11; numerical value otherwise.
+    :param card_one: str - card dealt. 'J', 'Q', 'K' = 10; 'A' = 1; numerical value otherwise.
+    :param card_two: str - card dealt. 'J', 'Q', 'K' = 10; 'A' = 1; numerical value otherwise.
     :return: bool - if the hand is a blackjack (two cards worth 21).
     """
 
     return (card_one == 'A' or card_two == 'A') and (card_one in CARDS_10 or card_two in CARDS_10)
 
 
-def can_split_pairs(card_one, card_two):
+def can_split_pairs(card_one: str, card_two: str) -> bool:
     """Determine if a player can split their hand into two hands.
 
-    :param card_one, card_two: str - cards dealt.
+    :param card_one: str - card dealt. 'J', 'Q', 'K' = 10; 'A' = 1; numerical value otherwise.
+    :param card_two: str - card dealt. 'J', 'Q', 'K' = 10; 'A' = 1; numerical value otherwise.
     :return: bool - if the hand can be split into two pairs (i.e. cards are of the same value).
     """
 
     return value_of_card(card_one) == value_of_card(card_two)
 
 
-def can_double_down(card_one, card_two):
+def can_double_down(card_one: str, card_two: str) -> bool:
     """Determine if a blackjack player can place a double down bet.
 
-    :param card_one, card_two: str - first and second cards in hand.
+    :param card_one: str - card dealt. 'J', 'Q', 'K' = 10; 'A' = 1; numerical value otherwise.
+    :param card_two: str - card dealt. 'J', 'Q', 'K' = 10; 'A' = 1; numerical value otherwise.
     :return: bool - if the hand can be doubled down (i.e. totals 9, 10 or 11 points).
     """
 
