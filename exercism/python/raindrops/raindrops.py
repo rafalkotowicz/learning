027175ae1e712eb7a9fbee149418da684948b71c
Rafał Kotowicz <rafal.the.cat@gmail.com>
2022-03-20 08:47:46 +0100

    [Exercism][Python][Raindrops] Done. Loops, Strings, Modulo.

---
+++ exercism/python/raindrops/raindrops.py
@@ -0,0 +1,10 @@
+def convert(number: int) -> str:
+    sound = ""
+    if number % 3 == 0:
+        sound += "Pling"
+    if number % 5 == 0:
+        sound += "Plang"
+    if number % 7 == 0:
+        sound += "Plong"
+
+    return str(number) if not sound else sound
