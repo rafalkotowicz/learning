fa0bea17380b3272ec3815ee074c3c867aaf1cbb
Rafał Kotowicz <rafal.the.cat@gmail.com>
2022-04-04 13:08:34 +0200

    [Exercism][Python][ReverseString] Done.

---
+++ exercism/python/reverse-string/reverse_string.py
@@ -0,0 +1,8 @@
+def reverse(text):
+    return text[::-1]
+
+# def reverse(text):
+#     reversed_string = ""
+#     for index, char in enumerate(text):
+#         reversed_string += text[-(index + 1)]
+#     return reversed_string
