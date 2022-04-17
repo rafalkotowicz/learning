c7bd0b6fc4a9e0f64a70b8618d8c85286621ba07
Rafał Kotowicz <rafal.the.cat@gmail.com>
2022-04-18 01:26:49 +0200

    [Exercism][Python][RobotName] Done. Classes, Random, UUID.

---
+++ exercism/python/robot-name/robot_name.py
@@ -0,0 +1,18 @@
+import random
+import string
+from uuid import uuid4
+
+
+def name_generator():
+    random.seed(uuid4())
+    random_string = random.choice(string.ascii_letters) + random.choice(string.ascii_letters)
+    random_number = random.randint(100, 999)
+    return random_string.upper() + str(random_number)
+
+
+class Robot:
+    def __init__(self):
+        self.name = name_generator()
+
+    def reset(self):
+        self.name = name_generator()
