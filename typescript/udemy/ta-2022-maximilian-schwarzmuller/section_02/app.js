d0933143829af2ed451ecc018f5fa2922bfe35af
Rafał Kotowicz <rafal.the.cat@gmail.com>
2022-06-04 23:16:08 +0200

    [Udemy][TypeScript][Maximilian-Schwarzmuller][Section_02] Minor follow up to Section 02

--- typescript/udemy/ta-2022-maximilian-schwarzmuller/section_02/app.js
+++ typescript/udemy/ta-2022-maximilian-schwarzmuller/section_02/app.js
@@ -1,5 +1,12 @@
-var person = {
-    name: "Rafal",
-    age: 35
-};
-console.log(person.age);
+var userInput;
+var userName;
+userInput = 5;
+userInput = 'Max';
+if (typeof userInput === 'string') {
+    userName = userInput;
+}
+function generateError(message, code) {
+    throw { message: message, errorCode: code };
+    // while (true) {}
+}
+generateError('An error occurred!', 500);
