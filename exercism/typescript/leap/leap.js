68232c050b4cae66a1a7908e96c8a16dc4707419
Rafał Kotowicz <rafal.the.cat@gmail.com>
2022-07-03 00:18:30 +0200

    [Exercism][TypeScript][Leap] Done

---
+++ exercism/typescript/leap/leap.js
@@ -0,0 +1,14 @@
+export function isLeap(year) {
+    if (year % 400 === 0) {
+        return true;
+    }
+    else if (year % 100 === 0) {
+        return false;
+    }
+    else
+        return year % 4 === 0;
+}
+// if (year % 400 === 0 || (year % 4 === 0 && year % 100 !== 0)) {
+//   return true;
+// } else
+//   return false;
