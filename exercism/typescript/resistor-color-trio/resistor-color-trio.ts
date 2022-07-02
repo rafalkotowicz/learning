f6fcfe8dfbe51061cffd9091aaeab206dd66a344
Rafał Kotowicz <rafal.the.cat@gmail.com>
2022-07-03 00:03:42 +0200

    [Exercism][TypeScript][ResistorColorTrio] Done

---
+++ exercism/typescript/resistor-color-trio/resistor-color-trio.ts
@@ -0,0 +1,26 @@
+export const COLORS = ["black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white"];
+
+export function decodedResistorValue(bandsColors: string[]): string {
+  let resistance: string = "";
+  for (let i = 0; i <= bandsColors.length && i < 2; i++) {
+    resistance += "" + COLORS.indexOf(bandsColors[i].toLowerCase());
+  }
+
+  let zeros: string = "";
+  if (bandsColors[2] !== null) {
+    for (let i = 0; i < COLORS.indexOf(bandsColors[2].toLowerCase()); i++) {
+      zeros += "0";
+    }
+  }
+
+  let resistanceValue: number = parseInt(resistance + zeros);
+  let unit: string;
+  if (resistanceValue % 1000 === 0) {
+    resistanceValue /= 1000;
+    unit = "kiloohms";
+  } else {
+    unit = "ohms";
+  }
+
+  return resistanceValue + " " + unit;
+}
