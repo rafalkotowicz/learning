4ba75f8b3691ba3db8ed700401c91a9d3b24c34d
Rafał Kotowicz <rafal.the.cat@gmail.com>
2022-07-09 14:36:18 +0200

    [Exercism][TypeScript][SecretHandshake] Done.

---
+++ exercism/typescript/secret-handshake/secret-handshake.ts
@@ -0,0 +1,24 @@
+export function commands(input: number): string[] {
+  let result: string[] = [];
+  let binaries: number[] = Number(input).toString(2).split("").map(x => parseInt(x)).reverse();
+
+  for (let pos = 0; pos < binaries.length; pos++) {
+    if (pos === 0 && binaries[pos] === 1) {
+      result.push("wink");
+    }
+    if (pos === 1 && binaries[pos] === 1) {
+      result.push("double blink");
+    }
+    if (pos === 2 && binaries[pos] === 1) {
+      result.push("close your eyes");
+    }
+    if (pos === 3 && binaries[pos] === 1) {
+      result.push("jump");
+    }
+    if (pos === 4 && binaries[pos] === 1) {
+      result.reverse();
+    }
+  }
+
+  return result;
+}
