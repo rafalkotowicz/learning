9b8229270a97cffbd341e94513a27777bf40ab6a
Rafał Kotowicz <rafal.the.cat@gmail.com>
2022-07-14 23:36:51 +0200

    [Exercism][TypeScript][CollatzConjecture] Done.

---
+++ exercism/typescript/collatz-conjecture/collatz-conjecture.js
@@ -0,0 +1,16 @@
+export function steps(n) {
+    if (n <= 0) {
+        throw new Error("Only positive numbers are allowed");
+    }
+    let steps = 0;
+    while (n !== 1) {
+        if (n % 2 === 0) {
+            n = n / 2;
+        }
+        else {
+            n = n * 3 + 1;
+        }
+        steps += 1;
+    }
+    return steps;
+}
