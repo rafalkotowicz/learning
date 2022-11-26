9e8acc199ac2db80b063e4d1d06a5bed0fedf569
Rafał Kotowicz <rafal.the.cat@gmail.com>
2022-11-26 21:53:55 +0100

    [Exercism][Python][RailFenceCipher] Decode - concept; Not done yet

--- exercism/python/rail-fence-cipher/rail_fence_cipher.py
+++ exercism/python/rail-fence-cipher/rail_fence_cipher.py
@@ -2,7 +2,7 @@ import math
 
 
 def encode(message: str, rails: int) -> str:
-    encoded: [[str]] = [''] * rails
+    encoded: [str] = [''] * rails
     window = 2 * rails - 2
 
     for id, letter in enumerate(message):
@@ -17,4 +17,19 @@ def encode(message: str, rails: int) -> str:
 
 
 def decode(encoded_message: str, rails: int) -> str:
-    pass
+    rail_lengths: [int] = [0]*rails
+    window = 2 * rails - 2
+    decoded_message: str = ''
+    # cut encoded_message into pieces -> rail_lengths
+
+    for id in range(len(encoded_message)):
+        if id % window < rails:
+            # print(f'id: {id}, rail: {id % window}, letter: {letter}')
+            decoded_message += rail_lengths[id % window]
+        else:
+            # print(f'id: {id}, rail: {-(id % window - window // 2)}, letter: {letter}')
+            decoded_message += rail_lengths[-(id % window - window // 2) - 1]
+
+
+
+    return decoded_message
