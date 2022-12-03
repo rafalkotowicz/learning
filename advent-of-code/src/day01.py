38247b03b15dc257a6352176c84eb05c9bfcf4c3
Rafał Kotowicz <rafal.the.cat@gmail.com>
2022-12-03 01:56:25 +0100

    [AdventOfCode][2022][Python] Day 1 & 2

---
+++ advent-of-code/src/day01.py
@@ -0,0 +1,21 @@
+with open('day01.txt') as f:
+    lines = f.readlines()
+
+    elf_calories = []
+    sum = 0
+    for line in lines:
+        if line == '\n':
+            elf_calories.append(sum)
+            sum = 0
+        else:
+            sum += int(line.replace('\n',''))
+
+    first = max(elf_calories)
+    elf_calories.remove(first)
+    second = max(elf_calories)
+    elf_calories.remove(second)
+    third = max(elf_calories)
+    print(first + second + third)
+
+
+
---
+++ advent-of-code/src/day02.py
@@ -0,0 +1,55 @@
+ROCK = ['A', 'X']
+PAPER = ['B', 'Y']
+SCISSORS = ['C', 'Z']
+
+shape_score = {
+    'A': 1,
+    'X': 1,
+    'B': 2,
+    'Y': 2,
+    'C': 3,
+    'Z': 3
+}
+
+def match_points(match: [str]) -> int:
+    other, me = match
+    if me == 'X':
+        if other in ROCK:
+            me = 'C'
+        elif other in PAPER:
+            me = 'A'
+        else:
+            me = 'B'
+    elif me == 'Y':
+        me = other
+    else:
+        if other in ROCK:
+            me = 'B'
+        elif other in PAPER:
+            me = 'C'
+        else:
+            me = 'A'
+
+
+    if me in ROCK and other in ROCK or me in PAPER and other in PAPER or me in SCISSORS and other in SCISSORS:
+        return 3 + shape_score[me]
+    elif me in ROCK and other in SCISSORS or me in PAPER and other in ROCK or me in SCISSORS and other in PAPER:
+        return 6 + shape_score[me]
+    else:
+        return 0 + shape_score[me]
+
+
+with open('day02.txt') as f:
+    lines = f.readlines()
+    sum = 0
+    for line in lines:
+        sum += match_points(line.split())
+
+
+print(sum)
+
+
+
+# print(match_points(['A', 'Y']))
+# print(match_points(['B', 'X']))
+# print(match_points(['C', 'Z']))
