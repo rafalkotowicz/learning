ad2498cd98a913dc67bd0d16808a25c712311f78
Rafał Kotowicz <rafal.the.cat@gmail.com>
2023-04-01 13:30:02 +0200

    [Exercism][Rust][SemiStructuredLogs] Done. Adding packages, Crates.io.

--- exercism/rust/reverse-string/src/lib.rs
+++ exercism/rust/reverse-string/src/lib.rs
@@ -1,3 +1,6 @@
+use unicode_segmentation::UnicodeSegmentation;
+
 pub fn reverse(input: &str) -> String {
-    input.chars().rev().collect::<String>()
+    // input.chars().rev().collect::<String>()
+    input.graphemes(true).rev().collect::<String>() //graphemes
 }
