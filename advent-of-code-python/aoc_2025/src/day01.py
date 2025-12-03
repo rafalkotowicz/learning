f7a6138535367922fa9217ba0796448a20c577d7
Rafał Kotowicz <rafal.the.cat@gmail.com>
2025-12-03 22:31:58 +0100

    [AdventOfCode][2025][Python] Day 1 - Part 2 - DONE

--- advent-of-code-python/aoc_2025/src/day01.py
+++ advent-of-code-python/aoc_2025/src/day01.py
@@ -2,7 +2,7 @@ import math
 from turtledemo.sorting_animate import instructions1
 
 
-def puzzle_1(instructions):
+def puzzle_1(instructions: [int]) -> int:
     position = 50
     zero_hit = 0
     for instruction in instructions:
@@ -19,62 +19,33 @@ def puzzle_1(instructions):
 
     return zero_hit
 
-def puzzle_1_1(instructions):
+def puzzle_2(instructions: [str]) -> int:
     position = 50
     zero_hit = 0
-
     for instruction in instructions:
         direction = instruction[0]
         offset = int(instruction[1:])
 
         if direction == 'R':
-            position = position + offset
+            new_position = position + offset
+            zero_hit = zero_hit + rotate_and_count_zeros(position, 1, new_position)
+            position = new_position
         elif direction == 'L':
-            position = position - offset
+            new_position = position - offset
+            zero_hit = zero_hit + rotate_and_count_zeros(position, -1, new_position)
+            position = new_position
         else:
             raise ValueError(f'Invalid direction: {direction}')
 
-        if position == 0 or position == 100:
+    return zero_hit
+
+def rotate_and_count_zeros(start: int, direction: int, destination: int) -> int:
+    zero_hit = 0
+    current_position = start
+
+    while current_position != destination:
+        current_position = current_position + direction
+        if current_position % 100 == 0:
             zero_hit = zero_hit + 1
-            position = 0
-            continue
 
-        while position < 0 or position > 100:
-            if position > 100:
-                # zero_hit = zero_hit + 1
-                position = position - 100
-            elif position < 0:
-                # zero_hit = zero_hit + 1
-                position = position + 100
-
-    return zero_hit
-
-
-def puzzle_2(instructions):
-    position = 50
-    zero_hit = 0
-    for instruction in instructions:
-        direction = instruction[0]
-        offset = int(instruction[1:])
-
-        if direction == 'R':
-            position = position + offset
-        elif direction == 'L':
-            position = position - offset
-        else:
-            raise ValueError(f'Invalid direction: {direction}')
-
-        while position < 0 or position > 100:
-            if position >= 100:
-                if position == 100:
-                    zero_hit = zero_hit + 1
-                    position = 0
-                    break
-                else:
-                    zero_hit = zero_hit + 1
-                    position = position - 100
-            elif position < 0:
-                zero_hit = zero_hit + 1
-                position = position + 100
-
-    return zero_hit
+    return zero_hit;
--- advent-of-code-python/aoc_2025/test/test_day01.py
+++ advent-of-code-python/aoc_2025/test/test_day01.py
@@ -1,6 +1,6 @@
 import unittest
 
-from aoc_2025.src.day01 import puzzle_1, puzzle_2, puzzle_1_1
+from aoc_2025.src.day01 import puzzle_1, puzzle_2, rotate_and_count_zeros
 from utils.common import read_and_sanitize
 
 
@@ -23,15 +23,6 @@ class TestDay01Part01(unittest.TestCase):
         expected_value = 999
         self.assertEqual(expected_value, actual_value)
 
-    def test_puzzle_1_1(self):
-        instructions = read_and_sanitize('aoc_2025/test/resources/day01puzzle.txt')
-        actual_value = puzzle_1_1(instructions)
-        expected_value = 999
-        self.assertEqual(expected_value, actual_value)
-
-
-
-
 class TestDay01Part02(unittest.TestCase):
     def setUp(self) -> None:
         pass
@@ -48,13 +39,17 @@ class TestDay01Part02(unittest.TestCase):
     def test_puzzle_2(self):
         instructions = read_and_sanitize('aoc_2025/test/resources/day01puzzle.txt')
         actual_value = puzzle_2(instructions)
-        print(actual_value)
-        # expected_value = 999
-        # self.assertEqual(expected_value, actual_value)
-
+        expected_value = 6099
+        self.assertEqual(expected_value, actual_value)
         #6104 is too high
         #6002 is too low
 
+    def test_rotate_and_count_zeros(self):
+        self.assertEqual(1, rotate_and_count_zeros(0, +1, 100))
+        self.assertEqual(1, rotate_and_count_zeros(0, -1, -100))
+        self.assertEqual(0, rotate_and_count_zeros(1, +1, 99))
+        self.assertEqual(1, rotate_and_count_zeros(-1, +1, 0))
+        self.assertEqual(2, rotate_and_count_zeros(101, -1, 0))
 
 if __name__ == '__main__':
     unittest.main()
