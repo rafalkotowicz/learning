5b05115c18c87d431cc3818cf68ebaa9a75a6ec6
Rafał Kotowicz <rafal.the.cat@gmail.com>
2026-03-08 20:32:22 +0100

    [Exercism][Python][PalindromeProducts] Improving performance by 99%. Algorithm was the key.

--- exercism/python/palindrome-products/palindrome_products.py
+++ exercism/python/palindrome-products/palindrome_products.py
@@ -1,5 +1,3 @@
-# UNFINISHED: needs performance improvement (works, but is too slow)
-
 def validate_inputs(max_factor: int, min_factor: int = 0) -> None:
     if min_factor > max_factor:
         raise ValueError("min must be <= max")
@@ -19,52 +17,24 @@ def largest(max_factor: int, min_factor: int = 0) -> tuple[int | None, list[tupl
     :return: tuple of (palindrome, iterable).
              Iterable should contain both factors of the palindrome in an arbitrary order.
     """
-
     validate_inputs(max_factor, min_factor)
+    largest_palindrome = None
+    factors = []
+    for number_a in range(max_factor, min_factor - 1, -1):
+        was_bigger = False
+        for number_b in range(max_factor, number_a - 1, -1):
+            current_product = number_a * number_b
+            if largest_palindrome is None or current_product >= largest_palindrome:
+                was_bigger = True
+                if is_palindrome(current_product):
+                    if largest_palindrome is None or current_product > largest_palindrome:
+                        factors = []
+                        largest_palindrome = current_product
+                    factors.append([number_a, number_b])
+        if not was_bigger:
+            break
+    return largest_palindrome, factors
 
-    results = dict()
-    palindromes = []
-    for num_a in range(min_factor, max_factor + 1):
-        for num_b in range(min_factor, max_factor + 1):
-            product = num_a * num_b
-            if is_palindrome(product):
-                if results.get(product):
-                    results[product].append((num_a, num_b))
-                else:
-                    results[product] = [(num_a, num_b)]
-                palindromes.append(product)
-
-    largest_palindrome = max(palindromes) if palindromes else None
-    if largest_palindrome:
-        return largest_palindrome, results[largest_palindrome]
-    else:
-        return None, []
-
-    
-    # results = dict()
-    # palindromes = []
-    # pairs = set()
-    # for num_a in range(min_factor, max_factor + 1):
-    #     for num_b in range(min_factor, max_factor + 1):
-    #         if num_a < num_b:
-    #             pairs.add((num_a, num_b))
-    #         else:
-    #             pairs.add((num_b, num_a))
-
-    # for pair in pairs:
-    #     product = pair[0] * pair[1]
-    #     if is_palindrome(product):
-    #         if results.get(product):
-    #             results[product].append(pair)
-    #         else:
-    #             results[product] = [pair]
-    #         palindromes.append(product)
-
-    # largest_palindrome = max(palindromes) if palindromes else None
-    # if largest_palindrome:
-    #     return largest_palindrome, results[largest_palindrome]
-    # else:
-    #     return None, []
 
 def smallest(max_factor: int, min_factor: int = 0) -> tuple[int | None, list[tuple[int, int]]]:
     """Given a range of numbers, find the smallest palindromes which
@@ -73,19 +43,23 @@ def smallest(max_factor: int, min_factor: int = 0) -> tuple[int | None, list[tup
     :param max_factor: int
     :param min_factor: int with a default value of 0
     :return: tuple of (palindrome, iterable).
-    Iterable should contain both factors of the palindrome in an arbitrary order.
+             Iterable should contain both factors of the palindrome in an arbitrary order.
     """
 
     validate_inputs(max_factor, min_factor)
-
-    factors: list[tuple[int, int]] = []
-    for num_a in range(min_factor, max_factor + 1):
-        for num_b in range(min_factor, max_factor + 1):
-            product = num_a * num_b
-            if is_palindrome(product):
-                for num_c in range(min_factor, max_factor + 1):
-                    for num_d in range(min_factor, max_factor + 1):
-                        if num_c * num_d == product:
-                            factors.append((num_c, num_d))
-                return product, factors
-    return None, factors
+    smallest_palindrome = None
+    factors = []
+    for number_a in range(min_factor, max_factor + 1):
+        was_smaller = False
+        for number_b in range(number_a, max_factor + 1):
+            current_product = number_a * number_b
+            if smallest_palindrome is None or current_product <= smallest_palindrome:
+                was_smaller = True
+                if is_palindrome(current_product):
+                    if smallest_palindrome is None or current_product < smallest_palindrome:
+                        factors = []
+                        smallest_palindrome = current_product
+                    factors.append([number_a, number_b])
+        if not was_smaller:
+            break
+    return smallest_palindrome, factors
