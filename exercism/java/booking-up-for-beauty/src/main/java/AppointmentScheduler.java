24cb82ecfc0d7b0ad9e90e748105dfd8174bd948
Rafał Kotowicz <rafal.the.cat@gmail.com>
2026-07-14 13:24:25 +0200

    [Exercism][Java][BookingUpForBeauty] Done - https://exercism.org/tracks/java/exercises/booking-up-for-beauty

---
+++ exercism/java/booking-up-for-beauty/src/main/java/AppointmentScheduler.java
@@ -0,0 +1,39 @@
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+
+class AppointmentScheduler {
+    public LocalDateTime schedule(String appointmentDateDescription) {
+        String[] timeParts = appointmentDateDescription.split("[/ :]");
+
+        LocalDateTime datetime = LocalDateTime.of(
+                Integer.parseInt(timeParts[2]),
+                Integer.parseInt(timeParts[0]),
+                Integer.parseInt(timeParts[1]),
+                Integer.parseInt(timeParts[3]),
+                Integer.parseInt(timeParts[4]),
+                Integer.parseInt(timeParts[5]));
+        return datetime;
+    }
+
+    public boolean hasPassed(LocalDateTime appointmentDate) {
+        return appointmentDate.isBefore(LocalDateTime.now());
+    }
+
+    public boolean isAfternoonAppointment(LocalDateTime appointmentDate) {
+        int hour = appointmentDate.getHour();
+        return hour >= 12 && hour < 18;
+    }
+
+    public String getDescription(LocalDateTime appointmentDate) {
+        DateTimeFormatter printer = DateTimeFormatter.ofPattern("EEEE, MMMM d, yyyy, 'at' h:mm a", Locale.ENGLISH);
+        String dateFormatted = appointmentDate.format(printer);
+
+        return "You have an appointment on " + dateFormatted + ".";
+    }
+
+    public LocalDate getAnniversaryDate() {
+        return LocalDate.of(LocalDate.now().getYear(), 9, 15);
+    }
+}
