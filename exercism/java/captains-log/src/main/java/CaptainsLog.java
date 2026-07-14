8657b7423c4d1d4701d7e8e4cab87a453560d236
Rafał Kotowicz <rafal.the.cat@gmail.com>
2026-07-14 14:30:37 +0200

    [Exercism][Java][CaptainsLog] Done - https://exercism.org/tracks/java/exercises/captains-log

---
+++ exercism/java/captains-log/src/main/java/CaptainsLog.java
@@ -0,0 +1,24 @@
+import java.util.Random;
+
+class CaptainsLog {
+
+    private static final char[] PLANET_CLASSES = new char[]{'D', 'H', 'J', 'K', 'L', 'M', 'N', 'R', 'T', 'Y'};
+
+    private Random random;
+
+    CaptainsLog(Random random) {
+        this.random = random;
+    }
+
+    char randomPlanetClass() {
+        return PLANET_CLASSES[random.nextInt(PLANET_CLASSES.length)];
+    }
+
+    String randomShipRegistryNumber() {
+        return "NCC-" + (random.nextInt(9000) + 1000);
+    }
+
+    double randomStardate() {
+        return 41000 + random.nextDouble() * 1000;
+    }
+}
