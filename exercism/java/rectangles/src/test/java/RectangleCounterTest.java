cd1a146de6b8162c547382c29b527941e65f9d85
Rafał Kotowicz <rafal.the.cat@gmail.com>
2026-07-24 19:36:52 +0200

    [Exercism][Java][Rectangles] Done: https://exercism.org/tracks/java/exercises/rectangles

---
+++ exercism/java/rectangles/src/test/java/RectangleCounterTest.java
@@ -0,0 +1,188 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class RectangleCounterTest {
+
+    private RectangleCounter rectangleCounter;
+
+    @BeforeEach
+    public void setUp() {
+        rectangleCounter = new RectangleCounter();
+    }
+
+    @Test
+    @DisplayName("no rows")
+    public void testInputWithNoRowsContainsNoRectangles() {
+        String[] inputGrid = new String[]{};
+
+        assertThat(rectangleCounter.countRectangles(inputGrid)).isEqualTo(0);
+    }
+
+    @Test
+    @DisplayName("no columns")
+    public void testInputWithNoColumnsContainsNoRectangles() {
+        String[] inputGrid = new String[]{""};
+
+        assertThat(rectangleCounter.countRectangles(inputGrid)).isEqualTo(0);
+    }
+
+    @Test
+    @DisplayName("no rectangles")
+    public void testNonTrivialInputWithNoRectangles() {
+        String[] inputGrid = new String[]{" "};
+
+        assertThat(rectangleCounter.countRectangles(inputGrid)).isEqualTo(0);
+    }
+
+    @Test
+    @DisplayName("one rectangle")
+    public void testInputWithOneRectangle() {
+        String[] inputGrid = new String[]{
+            "+-+",
+            "| |",
+            "+-+"
+        };
+
+        assertThat(rectangleCounter.countRectangles(inputGrid)).isEqualTo(1);
+    }
+
+    @Test
+    @DisplayName("two rectangles without shared parts")
+    public void testInputWithTwoRectanglesWithoutSharedEdges() {
+        String[] inputGrid = new String[]{
+            "  +-+",
+            "  | |",
+            "+-+-+",
+            "| |  ",
+            "+-+  "
+        };
+
+        assertThat(rectangleCounter.countRectangles(inputGrid)).isEqualTo(2);
+    }
+
+    @Test
+    @DisplayName("five rectangles with shared parts")
+    public void testInputWithFiveRectanglesWithSharedEdges() {
+        String[] inputGrid = new String[]{
+            "  +-+",
+            "  | |",
+            "+-+-+",
+            "| | |",
+            "+-+-+"
+        };
+
+        assertThat(rectangleCounter.countRectangles(inputGrid)).isEqualTo(5);
+    }
+
+    @Test
+    @DisplayName("rectangle of height 1 is counted")
+    public void testThatRectangleOfHeightOneIsCounted() {
+        String[] inputGrid = new String[]{
+            "+--+",
+            "+--+"
+        };
+
+        assertThat(rectangleCounter.countRectangles(inputGrid)).isEqualTo(1);
+    }
+
+    @Test
+    @DisplayName("rectangle of width 1 is counted")
+    public void testThatRectangleOfWidthOneIsCounted() {
+        String[] inputGrid = new String[]{
+            "++",
+            "||",
+            "++"
+        };
+
+        assertThat(rectangleCounter.countRectangles(inputGrid)).isEqualTo(1);
+    }
+
+    @Test
+    @DisplayName("1x1 square is counted")
+    public void testThatOneByOneSquareIsCounted() {
+        String[] inputGrid = new String[]{
+            "++",
+            "++"
+        };
+
+        assertThat(rectangleCounter.countRectangles(inputGrid)).isEqualTo(1);
+    }
+
+    @Test
+    @DisplayName("only complete rectangles are counted")
+    public void testThatIncompleteRectanglesAreNotCounted() {
+        String[] inputGrid = new String[]{
+            "  +-+",
+            "    |",
+            "+-+-+",
+            "| | -",
+            "+-+-+"
+        };
+
+        assertThat(rectangleCounter.countRectangles(inputGrid)).isEqualTo(1);
+    }
+
+    @Test
+    @DisplayName("rectangles can be of different sizes")
+    public void testThatRectanglesOfDifferentSizesAreAllCounted() {
+        String[] inputGrid = new String[]{
+            "+------+----+",
+            "|      |    |",
+            "+---+--+    |",
+            "|   |       |",
+            "+---+-------+"
+        };
+
+        assertThat(rectangleCounter.countRectangles(inputGrid)).isEqualTo(3);
+    }
+
+    @Test
+    @DisplayName("corner is required for a rectangle to be complete")
+    public void testThatIntersectionsWithoutCornerCharacterDoNotCountAsRectangleCorners() {
+        String[] inputGrid = new String[]{
+            "+------+----+",
+            "|      |    |",
+            "+------+    |",
+            "|   |       |",
+            "+---+-------+"
+        };
+
+        assertThat(rectangleCounter.countRectangles(inputGrid)).isEqualTo(2);
+    }
+
+    @Test
+    @DisplayName("large input with many rectangles")
+    public void testLargeInputWithManyRectangles() {
+        String[] inputGrid = new String[]{
+            "+---+--+----+",
+            "|   +--+----+",
+            "+---+--+    |",
+            "|   +--+----+",
+            "+---+--+--+-+",
+            "+---+--+--+-+",
+            "+------+  | |",
+            "          +-+"
+        };
+
+        assertThat(rectangleCounter.countRectangles(inputGrid)).isEqualTo(60);
+    }
+    
+    @Test
+    @DisplayName("rectangles must have four sides")
+    public void testRectanglesMustHaveFourSides() {
+        String[] inputGrid = new String[]{
+            "+-+ +-+",
+            "| | | |",
+            "+-+-+-+",
+            "  | |  ",
+            "+-+-+-+",
+            "| | | |",
+            "+-+ +-+"
+        };
+
+        assertThat(rectangleCounter.countRectangles(inputGrid)).isEqualTo(5);
+    }
+}
